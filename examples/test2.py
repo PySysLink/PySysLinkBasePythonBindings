b4991c9ddc547c22f5b8f396d099ffdb4ad4a51c
PelloUsabiaga <pellousabiaga@gmail.com>
2025-03-06 21:23:19 +0100

    Example updated to simulate with custom block. Nanobind warning memory leaks

--- examples/test2.py
+++ examples/test2.py
@@ -1,6 +1,7 @@
 from typing import Any, Dict
 import pysyslink_base
 
+
 class Saturation(pysyslink_base.ISimulationBlock):
     def __init__(self, block_configuration: Dict[str, Any], block_events_handler: pysyslink_base.IBlockEventsHandler):
         self.sample_time = pysyslink_base.SampleTime(pysyslink_base.SampleTimeType.inherited, supported_sample_time_types_for_inheritance=[pysyslink_base.SampleTimeType.continuous, pysyslink_base.SampleTimeType.discrete])
@@ -35,23 +36,27 @@ class Saturation(pysyslink_base.ISimulationBlock):
         return self.get_output_ports()
 
 
+class CustomBlockFactory(pysyslink_base.IBlockFactory):
+    def __init__(self):
+        super().__init__()
+
+    def create_block(self, block_configuration, block_events_handler):
+        if block_configuration["BlockClass"] == "Custom/Saturation":
+            return Saturation(block_configuration, block_events_handler)
+        else:
+            raise Exception("BlockClass {} not supported".format(block_configuration["BlockClass"]))
+
+
+
 # Set the default logger configuration and log level
 pysyslink_base.SpdlogManager.configure_default_logger()
 pysyslink_base.SpdlogManager.set_log_level(pysyslink_base.LogLevel.debug)
 
-# solver_config = pysyslink_base.SimulationOptions()
-# solver_config.solvers_configuration = {"kaixo" : 1.2}
 
 # Create a BlockEventsHandler
 block_events_handler = pysyslink_base.BlockEventsHandler()
 
-signal_value = pysyslink_base.SignalValue_double(0.0)
-print(issubclass(pysyslink_base.SignalValue_double, pysyslink_base.UnknownTypeSignal))
-print(isinstance(signal_value, pysyslink_base.UnknownTypeSignal))
-# Load the plugins using BlockTypeSupportPlugingLoader
-pluging_loader = pysyslink_base.BlockTypeSupportPlugingLoader()
-block_factories = pluging_loader.load_plugins("/usr/local/lib")
-
+# Test that saturation block works
 saturation = Saturation({"Name" : "saturation_1", "Id" : "saturation_1", "high_limit": 10, "low_limit": -10}, block_events_handler)
 
 print(saturation)
@@ -65,3 +70,57 @@ print(saturation.get_output_ports()[0].get_value().try_cast_to_typed().get_paylo
 saturation.get_input_ports()[0].set_value(pysyslink_base.SignalValue_double(12.0))
 saturation.compute_outputs_of_block(saturation.get_sample_time(), 0.0)
 print(saturation.get_output_ports()[0].get_value().try_cast_to_typed().get_payload())
+
+# Load the plugins using BlockTypeSupportPlugingLoader
+pluging_loader = pysyslink_base.BlockTypeSupportPlugingLoader()
+block_factories = pluging_loader.load_plugins("/usr/local/lib")
+block_factories["Custom"] = CustomBlockFactory()
+
+simulation_model = pysyslink_base.ModelParser.parse_from_yaml(
+                        "system_saturation.yaml", block_factories, block_events_handler
+                    )
+
+
+# Get the direct block chains and order them
+block_chains = simulation_model.get_direct_block_chains()
+ordered_blocks = simulation_model.order_block_chains_onto_free_order(block_chains)
+
+# Propagate sample times
+simulation_model.propagate_sample_times()
+
+# Set up simulation options
+simulation_options = pysyslink_base.SimulationOptions()
+simulation_options.start_time = 0.0
+simulation_options.stop_time = 10.0
+simulation_options.run_in_natural_time = False
+simulation_options.natural_time_speed_multiplier = 1
+simulation_options.block_ids_input_or_output_and_indexes_to_log = [
+    ("sum2", "output", 0),
+    ("acumulator1", "output", 0),
+    ("display1", "input", 0)
+]
+# simulation_options.solvers_configuration = {
+#     "default": {
+#         "Type": "odeint",
+#         "ControlledSolver": "runge_kutta_fehlberg78"
+#     }
+# }
+
+# Create a SimulationManager and run the simulation
+simulation_manager = pysyslink_base.SimulationManager(simulation_model, simulation_options)
+simulation_output = simulation_manager.run_simulation()
+
+# Access and print continuous values for integrator2/output
+continuous_values = simulation_output.signals["LoggedSignals"]["sum2/output/0"].try_cast_to_typed().values
+continuous_times = simulation_output.signals["LoggedSignals"]["sum2/output/0"].try_cast_to_typed().times
+
+for time, value in zip(continuous_times, continuous_values):
+    print(f"{time}: {value}")
+
+# Access and print continuous values for display1/input
+continuous_values_log = simulation_output.signals["LoggedSignals"]["display1/input/0"].try_cast_to_typed().values
+continuous_times_log = simulation_output.signals["LoggedSignals"]["display1/input/0"].try_cast_to_typed().times
+
+for time, value in zip(continuous_times_log, continuous_values_log):
+    print(f"{time}: {value}")
+    
